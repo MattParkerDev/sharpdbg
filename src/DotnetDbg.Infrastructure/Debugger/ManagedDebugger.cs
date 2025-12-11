8910fd5f92f1d3b7a4f56a230898e337b5041c27
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-12-12 00:53:19 +1000

    Get variables

--- src/DotnetDbg.Infrastructure/Debugger/ManagedDebugger.cs
+++ src/DotnetDbg.Infrastructure/Debugger/ManagedDebugger.cs
@@ -517,10 +517,11 @@ public partial class ManagedDebugger : IDisposable
 
         try
         {
+	        var corDebugFunction = localsScope.Frame.Function;
+	        var module = _modules[corDebugFunction.Module.BaseAddress];
+
 	        foreach (var (index, localVariableCorDebugValue) in localsScope.Frame.LocalVariables.Index())
 	        {
-		        var corDebugFunction = localsScope.Frame.Function;
-		        var module = _modules[corDebugFunction.Module.BaseAddress];
 		        var localVariableName = module.SymbolReader?.GetLocalVariableName(corDebugFunction.Token, index);
 		        if (localVariableName is null) continue; // Compiler generated locals will not be found. E.g. DefaultInterpolatedStringHandler
 		        var value = GetValueForCorDebugValue(localVariableCorDebugValue);
@@ -531,7 +532,45 @@ public partial class ManagedDebugger : IDisposable
 			        Type = localVariableCorDebugValue.Type.ToString(),
 			        VariablesReference = 0 // TODO: set if complex type
 		        };
+		        result.Add(variableInfo);
+	        }
+	        if (localsScope.Frame.Arguments.Length is 0) return result;
+	        var metadataImport = module.Module.GetMetaDataInterface().MetaDataImport;
 
+	        // localsScope.Frame.Arguments includes the implicit "this" parameter for instance methods,
+	        // but GetParamForMethodIndex does NOT include it - it is named by convention
+	        // so we need to check the method attributes to see if it's static or instance, to conditionally handle "this"
+	        var methodProps = metadataImport!.GetMethodProps(corDebugFunction.Token);
+	        var isStatic = (methodProps.pdwAttr & CorMethodAttr.mdStatic) != 0;
+	        if (isStatic is false)
+	        {
+		        var implicitThisValue = localsScope.Frame.Arguments[0];
+		        var variableInfo = new VariableInfo
+		        {
+			        Name = "this", // Hardcoded - 'this' has no metadata
+			        Value = GetValueForCorDebugValue(implicitThisValue),
+			        Type = implicitThisValue.Type.ToString(),
+			        VariablesReference = 0 // TODO: set if complex type
+		        };
+		        result.Add(variableInfo);
+	        }
+			var skipCount = isStatic ? 0 : 1; // Skip 'this' for instance methods, as we already handled it
+	        foreach (var (index, argumentCorDebugValue) in localsScope.Frame.Arguments.Skip(skipCount).Index())
+	        {
+		        var metadataIndex = isStatic ?  index + 1 : index;
+		        // index 0 is the return value, so we add 1 to get to the arguments
+		        var paramDef = metadataImport!.GetParamForMethodIndex(corDebugFunction.Token, metadataIndex + 1);
+		        var paramProps = metadataImport.GetParamProps(paramDef);
+		        var argumentName = paramProps.szName;
+		        if (argumentName is null) continue;
+		        var value = GetValueForCorDebugValue(argumentCorDebugValue);
+		        var variableInfo = new VariableInfo
+		        {
+			        Name = argumentName,
+			        Value = value,
+			        Type = argumentCorDebugValue.Type.ToString(),
+			        VariablesReference = 0 // TODO: set if complex type
+		        };
 		        result.Add(variableInfo);
 	        }
         }

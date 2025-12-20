4c9d482e88c371df7ecf6076070c051a396e734e
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-12-20 18:50:56 +1000

    Update DotnetDbgTests.cs

--- tests/DotnetDbg.Cli.Tests/DotnetDbgTests.cs
+++ tests/DotnetDbg.Cli.Tests/DotnetDbgTests.cs
@@ -455,7 +455,7 @@ public class DotnetDbgTests(ITestOutputHelper testOutputHelper)
 	    [
 		    new() {Name = "this", Value = "{DebuggableConsoleApp.MyClass}", Type = "DebuggableConsoleApp.MyClass", EvaluateName = "this", VariablesReference = 3 },
 		    new() {Name = "myParam", Value = "13", Type = "long", EvaluateName = "myParam" },
-		    new() {Name = "myInt", Value = "0", Type = "int", EvaluateName = "myInt" },
+		    new() {Name = "myInt", Value = "4", Type = "int", EvaluateName = "myInt" },
 		    new() {Name = "nullableInt", Value = "null", Type = "int?", EvaluateName = "nullableInt" },
 		    new() {Name = "nullableIntWithVal", Value = "4", Type = "int?", EvaluateName = "nullableIntWithVal" },
 		    new() {Name = "nullableRefType", Value = "null", Type = "DebuggableConsoleApp.MyClass", EvaluateName = "nullableRefType" },

36583767fc29f043499e3da1267ea5adaca3a4c0
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-12-23 14:57:40 +1000

    evaluator v0

---
+++ src/DotnetDbg.Infrastructure/Debugger/Eval/Evaluation.StackMachineProgram.cs
@@ -0,0 +1,12 @@
+﻿namespace DotnetDbg.Infrastructure.Debugger.Eval;
+
+public partial class Evaluation
+{
+	public class StackMachineProgram
+	{
+		public static readonly int ProgramFinished = -1;
+		public static readonly int BeforeFirstCommand = -2;
+		public int CurrentPosition = BeforeFirstCommand;
+		public List<ICommand> Commands = new List<ICommand>();
+	}
+}

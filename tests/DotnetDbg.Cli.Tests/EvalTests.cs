0bf84fc71feaebc24a2a0fc6165070fddc649064
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-12-27 00:51:23 +1000

    add test

--- tests/DotnetDbg.Cli.Tests/EvalTests.cs
+++ tests/DotnetDbg.Cli.Tests/EvalTests.cs
@@ -77,8 +77,9 @@ public class EvalTests(ITestOutputHelper testOutputHelper)
 	    // debugProtocolHost.WithEvaluateRequest(stackFrameId, "this.Get14() + 4", out var evaluateResponse11);
 	    // evaluateResponse11.Result.Should().Be("18");
 	    // TODO: Static class name as first identifier
-	    ;
 	    debugProtocolHost.WithEvaluateRequest(stackFrameId, "MyClass.IntStaticProperty + 4", out var evaluateResponse12);
 	    evaluateResponse12.Result.Should().Be("14");
+	    debugProtocolHost.WithEvaluateRequest(stackFrameId, "DebuggableConsoleApp.MyClass.IntStaticProperty + 4", out var evaluateResponse13);
+	    evaluateResponse13.Result.Should().Be("14");
     }
 }

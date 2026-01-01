afc655d8dd512c3453ba69ffa95330e95bebc2e2
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2026-01-01 12:24:18 +1000

    conditional exe path

--- tests/SharpDbg.Cli.Tests/Helpers/DebuggableProcessHelper.cs
+++ tests/SharpDbg.Cli.Tests/Helpers/DebuggableProcessHelper.cs
@@ -7,7 +7,7 @@ public static class DebuggableProcessHelper
 	public static Process StartDebuggableProcess(bool startSuspended = false)
 	{
 		var useShellExecute = !startSuspended;
-		var filePath = Path.JoinFromGitRoot("artifacts", "bin", "DebuggableConsoleApp", "debug", "DebuggableConsoleApp.exe");
+		var filePath = Path.JoinFromGitRoot("artifacts", "bin", "DebuggableConsoleApp", "debug", OperatingSystem.IsWindows() ? "DebuggableConsoleApp.exe" : "DebuggableConsoleApp");
 		if (File.Exists(filePath) is false) throw new FileNotFoundException("DebuggableConsoleApp executable not found", filePath);
 		var process = new Process
 		{

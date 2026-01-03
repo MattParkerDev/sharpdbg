ff49844e17672a1f2f481fdde2f1c5aaa50d3a81
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2026-01-03 15:45:24 +1000

    update test

--- tests/SharpDbg.Cli.Tests/AsyncStepTests.cs
+++ tests/SharpDbg.Cli.Tests/AsyncStepTests.cs
@@ -18,18 +18,41 @@ public class AsyncStepTests(ITestOutputHelper testOutputHelper)
 		    .WithAttachRequest(p2.Id)
 		    .WaitForInitializedEvent(initializedEventTcs);
 	    debugProtocolHost
-		    .WithBreakpointsRequest(10, Path.JoinFromGitRoot("tests", "DebuggableConsoleApp", "MyAsyncClass.cs"))
+		    .WithBreakpointsRequest(9, Path.JoinFromGitRoot("tests", "DebuggableConsoleApp", "MyAsyncClass.cs"))
 		    .WithConfigurationDoneRequest()
 		    .WithOptionalResumeRuntime(p2.Id, startSuspended);
 
 	    var stoppedEvent = await debugProtocolHost.WaitForStoppedEvent(stoppedEventTcs);
 	    var stopInfo = stoppedEvent.ReadStopInfo();
 	    stopInfo.filePath.Should().EndWith("MyAsyncClass.cs");
-	    stopInfo.line.Should().Be(10);
+	    stopInfo.line.Should().Be(9);
+
+	    debugProtocolHost.WithClearBreakpointsRequest(Path.JoinFromGitRoot("tests", "DebuggableConsoleApp", "MyAsyncClass.cs"));
 
 	    var stoppedEvent2 = await debugProtocolHost.WithStepOverRequest(stoppedEvent.ThreadId!.Value).WaitForStoppedEvent(stoppedEventTcs);
 	    var stopInfo2 = stoppedEvent2.ReadStopInfo();
 	    stopInfo2.filePath.Should().EndWith("MyAsyncClass.cs");
-	    stopInfo2.line.Should().Be(11);
+	    stopInfo2.line.Should().Be(10);
+
+	    var stoppedEvent3 = await debugProtocolHost.WithStepOverRequest(stoppedEvent.ThreadId!.Value).WaitForStoppedEvent(stoppedEventTcs);
+	    var stopInfo3 = stoppedEvent3.ReadStopInfo();
+	    stopInfo3.filePath.Should().EndWith("MyAsyncClass.cs");
+	    stopInfo3.line.Should().Be(11);
+
+	    var stoppedEvent4 = await debugProtocolHost.WithStepOverRequest(stoppedEvent.ThreadId!.Value).WaitForStoppedEvent(stoppedEventTcs);
+	    var stopInfo4 = stoppedEvent4.ReadStopInfo();
+	    stopInfo4.filePath.Should().EndWith("MyAsyncClass.cs");
+	    stopInfo4.line.Should().Be(12);
+
+	    return;
+
+	    foreach (var i in Enumerable.Range(0, 20))
+	    {
+		    var stoppedEventx = await debugProtocolHost.WithStepOverRequest(stoppedEvent.ThreadId!.Value).WaitForStoppedEvent(stoppedEventTcs);
+		    var stopInfox = stoppedEventx.ReadStopInfo();
+		    stoppedEventx.ThreadId!.Value.Should().Be(stoppedEvent.ThreadId.Value);
+		    stopInfox.line.Should().Be(9);
+		    ;
+	    }
     }
 }

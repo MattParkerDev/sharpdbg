4b1fc9ec5f72b2680ab227d14cc87bb4d6263774
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2026-01-05 18:37:08 +1000

    Create JmcConstants.cs

---
+++ src/SharpDbg.Infrastructure/Debugger/JmcConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace SharpDbg.Infrastructure.Debugger;
+
+public static class JmcConstants
+{
+	public static readonly string[] JmcTypeAttributeNames =
+	[
+		"System.Diagnostics.DebuggerNonUserCodeAttribute",
+		"System.Diagnostics.DebuggerStepThroughAttribute",
+	];
+	public static readonly string[] JmcMethodAttributeNames =
+	[
+		"System.Diagnostics.DebuggerNonUserCodeAttribute",
+		"System.Diagnostics.DebuggerStepThroughAttribute",
+		"System.Diagnostics.DebuggerHiddenAttribute"
+	];
+}

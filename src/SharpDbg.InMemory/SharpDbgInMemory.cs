1780c8143eaaaad9c17a3d93d8c61d4e8ef442fb
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2026-01-06 22:31:31 +1000

    add in memory project

---
+++ src/SharpDbg.InMemory/SharpDbgInMemory.cs
@@ -0,0 +1,45 @@
+﻿using System.IO.Pipes;
+using SharpDbg.Application;
+
+namespace SharpDbg.InMemory;
+
+public static class SharpDbgInMemory
+{
+	public static (Stream Input, Stream Output) NewDebugAdapterStreams()
+	{
+		var (input, output, _) = InMemoryDebugAdapterHelper.GetAdapterStreams();
+		return  (input, output);
+	}
+}
+
+internal static class InMemoryDebugAdapterHelper
+{
+	public static (AnonymousPipeServerStream input, AnonymousPipeClientStream output, DebugAdapter debugAdapter) GetAdapterStreams()
+	{
+		var stdInServer = new AnonymousPipeServerStream(PipeDirection.Out); // write
+		var stdInClient = new AnonymousPipeClientStream(PipeDirection.In, stdInServer.ClientSafePipeHandle); // std in read
+
+		var stdOutServer = new AnonymousPipeServerStream(PipeDirection.Out); // write
+		var stdOutClient = new AnonymousPipeClientStream(PipeDirection.In, stdOutServer.ClientSafePipeHandle); // std out read
+
+		var adapter = new DebugAdapter(Log);
+		adapter.Initialize(stdInClient, stdOutServer);
+		adapter.Protocol.VerifySynchronousOperationAllowed();
+		adapter.Protocol.Run();
+		_ = Task.Run(() =>
+		{
+			adapter.Protocol.WaitForReader();
+			stdInServer.Dispose();
+			stdInClient.Dispose();
+			stdOutServer.Dispose();
+			stdOutClient.Dispose();
+		});
+
+		return (stdInServer, stdOutClient, adapter);
+
+		void Log(string message)
+		{
+			//testOutputHelper.WriteLine($"Log [SharpDbg]: {message}");
+		}
+	}
+}

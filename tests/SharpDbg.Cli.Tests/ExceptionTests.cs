ab0eb6970e33d6cec75d54eca35bc7620f68fd9f
GustavEikaas <gustav.eikaas@gmail.com>
2026-06-13 09:40:52 +0200

    fix: tests not working cross platform

--- tests/SharpDbg.Cli.Tests/ExceptionTests.cs
+++ tests/SharpDbg.Cli.Tests/ExceptionTests.cs
@@ -1,4 +1,4 @@
-﻿using AwesomeAssertions;
+using AwesomeAssertions;
 using Microsoft.VisualStudio.Shared.VSCodeDebugProtocol.Messages;
 using SharpDbg.Cli.Tests.Helpers;
 
@@ -51,9 +51,9 @@ public class ExceptionTests(ITestOutputHelper testOutputHelper)
 		scopesResponse2.Scopes.Should().HaveCount(1);
 		var scope = scopesResponse2.Scopes.Single();
 
-		List<Variable> expectedVariables =
-		[
-			new() { Name = "$exception",  EvaluateName = "$exception",  Value = $$"""System.InvalidOperationException: Test exception{{"\r\n"}}   at DebuggableConsoleApp.Exceptions.Test(Boolean shouldThrow) in {{breakpointedFilePath}}:line 12""", Type = "System.InvalidOperationException", VariablesReference = 4 },
+			List<Variable> expectedVariables =
+			[
+			new() { Name = "$exception",  EvaluateName = "$exception",  Value = $"System.InvalidOperationException: Test exception{Environment.NewLine}   at DebuggableConsoleApp.Exceptions.Test(Boolean shouldThrow) in {breakpointedFilePath}:line 12", Type = "System.InvalidOperationException", VariablesReference = 4 },
 			new() { Name = "shouldThrow", EvaluateName = "shouldThrow", Value = "true",  Type = "bool" },
 			new() { Name = "test", EvaluateName = "test", Value = "true",  Type = "bool" },
 		];
--- tests/SharpDbg.Cli.Tests/Variables2Tests.cs
+++ tests/SharpDbg.Cli.Tests/Variables2Tests.cs
@@ -35,6 +35,7 @@ public class Variables2Tests(ITestOutputHelper testOutputHelper)
 
 		scopesResponse.Scopes.Should().HaveCount(1);
 		var scope = scopesResponse.Scopes.Single();
+		var expectedLocalDateTime = new DateTime(2026, 6, 13, 5, 42, 39).ToString();
 
 		List<Variable> expectedVariables =
 		[
@@ -65,16 +66,16 @@ public class Variables2Tests(ITestOutputHelper testOutputHelper)
 			new() { VariablesReference = 7,  Name = "localDictionary",      	EvaluateName = "localDictionary",      		Value = "Count = 1",								Type = "System.Collections.Generic.Dictionary<int, string>" },
 			new() { VariablesReference = 8,  Name = "localStruct",          	EvaluateName = "localStruct",          		Value = "{DebuggableConsoleApp.TestStruct}",		Type = "DebuggableConsoleApp.TestStruct" },
 			new() { VariablesReference = 9,  Name = "localClass",           	EvaluateName = "localClass",           		Value = "{DebuggableConsoleApp.TestClass}",			Type = "DebuggableConsoleApp.TestClass" },
-			new() { VariablesReference = 10, Name = "localRecord",          	EvaluateName = "localRecord",          		Value = "{TestRecord { Name = record, Age = 1 }}",	Type = "DebuggableConsoleApp.TestRecord" },
-			new() { VariablesReference = 11, Name = "localInterface",       	EvaluateName = "localInterface",       		Value = "{DebuggableConsoleApp.TestClass}",			Type = "DebuggableConsoleApp.ITestInterface {DebuggableConsoleApp.TestClass}" },
-			new() { VariablesReference = 12, Name = "localDelegate",        	EvaluateName = "localDelegate",        		Value = "{System.Func<int, int>}",					Type = "System.Func<int, int>" },
-			new() { VariablesReference = 13, Name = "localTuple",           	EvaluateName = "localTuple",           		Value = "{(1, stringInTuple)}",                 	Type = "System.Tuple<int, string>" },
-			new() { VariablesReference = 14, Name = "localValueTuple",      	EvaluateName = "localValueTuple",      		Value = "(2, \"stringInValueTuple\")",				Type = "(int A, string B)" },
-			new() { VariablesReference = 15, Name = "localGeneric",         	EvaluateName = "localGeneric",         		Value = "{DebuggableConsoleApp.GenericBox<int>}",	Type = "DebuggableConsoleApp.GenericBox<int>" },
-			new() { VariablesReference = 0,  Name = "localDynamic",         	EvaluateName = "localDynamic",         		Value = "241",                                  	Type = "object {int}" },
-			new() { VariablesReference = 16, Name = "localAnonymous",       	EvaluateName = "localAnonymous",       		Value = "{ Id = 1, Name = \"Anonymous\" }",         Type = "<Anonymous Type>" },
-			new() { VariablesReference = 17, Name = "localDateTime",        	EvaluateName = "localDateTime",        		Value = "13/06/2026 5:42:39 AM",					Type = "System.DateTime" },
-			new() { VariablesReference = 18, Name = "localGuid",            	EvaluateName = "localGuid",            		Value = "27de5b68-af24-4e59-a785-dde52e2ea7af",		Type = "System.Guid" },
+				new() { VariablesReference = 10, Name = "localRecord",          	EvaluateName = "localRecord",          		Value = "TestRecord { Name = record, Age = 1 }",	Type = "DebuggableConsoleApp.TestRecord" },
+				new() { VariablesReference = 11, Name = "localInterface",       	EvaluateName = "localInterface",       		Value = "{DebuggableConsoleApp.TestClass}",			Type = "DebuggableConsoleApp.TestClass" },
+				new() { VariablesReference = 12, Name = "localDelegate",        	EvaluateName = "localDelegate",        		Value = "{System.Func<int, int>}",					Type = "System.Func<int, int>" },
+				new() { VariablesReference = 13, Name = "localTuple",           	EvaluateName = "localTuple",           		Value = "(1, stringInTuple)",                 		Type = "System.Tuple<int, string>" },
+				new() { VariablesReference = 14, Name = "localValueTuple",      	EvaluateName = "localValueTuple",      		Value = "(2, stringInValueTuple)",					Type = "System.ValueTuple<int, string>" },
+				new() { VariablesReference = 15, Name = "localGeneric",         	EvaluateName = "localGeneric",         		Value = "{DebuggableConsoleApp.GenericBox<int>}",	Type = "DebuggableConsoleApp.GenericBox<int>" },
+				new() { VariablesReference = 16, Name = "localDynamic",         	EvaluateName = "localDynamic",         		Value = "241",                                  	Type = "System.Int32" },
+				new() { VariablesReference = 17, Name = "localAnonymous",       	EvaluateName = "localAnonymous",       		Value = "{{ Id = 1, Name = Anonymous }}",         	Type = "<>f__AnonymousType0<int, string>" },
+				new() { VariablesReference = 18, Name = "localDateTime",        	EvaluateName = "localDateTime",        		Value = expectedLocalDateTime,						Type = "System.DateTime" },
+				new() { VariablesReference = 19, Name = "localGuid",            	EvaluateName = "localGuid",            		Value = "27de5b68-af24-4e59-a785-dde52e2ea7af",		Type = "System.Guid" },
 		];
 
 		debugProtocolHost.WithVariablesRequest(scope.VariablesReference, out var variables);

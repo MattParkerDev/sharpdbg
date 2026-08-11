bce6006a5ea1942a1d160453dc7be1a20fa04783
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2026-08-11 20:21:48 +1000

    remove

--- src/SharpDbg.Infrastructure/Debugger/ExpressionEvaluator/CompiledExpression.cs
+++
@@ -1,8 +0,0 @@
-using SharpDbg.Infrastructure.Debugger.ExpressionEvaluator.Compiler;
-
-namespace SharpDbg.Infrastructure.Debugger.ExpressionEvaluator;
-
-public class CompiledExpression(List<CommandBase> instructions)
-{
-	public List<CommandBase> Instructions { get; set; } = instructions;
-}

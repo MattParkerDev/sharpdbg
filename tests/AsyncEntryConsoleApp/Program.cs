943a78552fb98e65789dbae881359ce40f4de39f
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2026-09-02 00:08:50 +1000

    more debuggee projects

---
+++ tests/AsyncEntryConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿
+internal class Program
+{
+	public static async Task Main(string[] args)
+	{
+		var test = 4;
+		await Task.Delay(10);
+	}
+}
---
+++ tests/AsyncTopLevelStatementsConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿var test = 4;
+test = 5;
+
+var myLambda = (int x) => x + test;
+await Task.Delay(10);
